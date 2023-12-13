2a58457032e7840418869ca3068937d4d23947a6
Markel Sainz <msainz@ikerlan.es>
2023-12-13 20:50:53 +0100

    Updates in proj2 to test debugging in vscode

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
+    int fav_number {100};
     cout << "Hello from Proj2" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> fav_number;
+    cout << "Your favorite number is: " << fav_number << endl;
     return 0;
 }
